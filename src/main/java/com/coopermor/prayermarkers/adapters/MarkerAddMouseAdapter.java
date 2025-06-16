304423e2c1bbddafa679a4d050f11870df762cac
nucleon <4823135+nucleon@users.noreply.github.com>
2025-06-16 17:43:32 -0400

    Setup Swing Layout & Add Marker Label

---
+++ src/main/java/com/coopermor/prayermarkers/adapters/MarkerAddMouseAdapter.java
@@ -0,0 +1,41 @@
+package com.coopermor.prayermarkers.adapters;
+
+import com.coopermor.prayermarkers.PrayerMarkersPlugin;
+import net.runelite.client.util.ImageUtil;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+public class MarkerAddMouseAdapter extends MouseAdapter {
+    private static final BufferedImage addIcon = ImageUtil.loadImageResource(PrayerMarkersPlugin.class, "add_icon.png");
+    private static final ImageIcon ADD_ICON = new ImageIcon(addIcon);
+    private static final ImageIcon ADD_HOVER_ICON = new ImageIcon(ImageUtil.alphaOffset(addIcon, 0.53f));
+    private final JLabel markerAdd;
+    private final Runnable onClick;
+
+    public MarkerAddMouseAdapter(JLabel markerAdd, Runnable onClick)
+    {
+        this.markerAdd = markerAdd;
+        this.onClick = onClick;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+        onClick.run();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+        markerAdd.setIcon(ADD_HOVER_ICON);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+        markerAdd.setIcon(ADD_ICON);
+    }
+}
